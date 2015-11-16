5ed2b4b10d09b8653d806e7813433901ecc9d21a
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2015-11-16 13:35:34 +0100

    CAN DO TRIANGLE PLOT FOR SEVERAL CHAINS IN THE SAME FOLDER REMOVING BURN-IN PHASE. CAN DO 1D PLOTS WITH VERTICAL MARKER FOR FIDUCIAL MODEL

--- analyzer/analyze.py
+++ analyzer/analyze.py
@@ -2,7 +2,7 @@ from getdist import loadMCSamples,plots
 
 number_of_parameters = 7
 
-samples = loadMCSamples('../output/chains/mcmc_final_output',settings={'ignore_rows':2})
+samples = loadMCSamples('../output/chains/mcmc_final_output',settings={'ignore_rows':0.2})
 
 g = plots.getSinglePlotter()
 
@@ -38,4 +38,13 @@ stats.saveAsText('../output/chains/1Dstatistics.txt')
 
 print 'FIGURE HAS BEEN CREATED'
 
+f = plots.getSubplotPlotter()
+
+f.settings.rcSizes(axes_fontsize = 2,lab_fontsize = 7)
+
+f.plots_1d(samples,['omega_b','omega_cdm','n_s','A_s','H0','m_ncdm','MG_beta2'],markers=[2.225e-2,1.198e-1,9.645e-1,2.20652e-9,6.727e1,6.0e-2,1.],nx=3)
+
+f.export('../output/chains/1D_plots.pdf')
+
 exit()
+
