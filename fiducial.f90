6964bea2e984773714c862039761900bd6aa3dd2
Wilmar Alberto Cardona Castro <wilmar.cardona@unige.ch>
2016-02-28 20:33:57 +0100

    RANGES AND PRECISION UPDATED

--- fiducial.f90
+++ fiducial.f90
@@ -56,11 +56,11 @@ Module fiducial
   Real*8,parameter    :: fsky = 1.5d4/4.1253d4
   Real*8,parameter    :: theoreticalerror = 0.d0 !5.d-2
   Real*8,parameter    :: l_switch_limber_for_cl_density_over_z = 20000.d0
-  Real*8,parameter    :: selection_sampling_bessel_fid = 0.5d0 !3.d0 ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
-  Real*8,parameter    :: q_linstep_fid = 1.d0 !0.3d0                ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
-  Real*8,parameter    :: k_max_tau0_over_l_max_fid = 2.d0 !20.d0    ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
-  Real*8,parameter    :: selection_sampling_bessel_mcmc = 0.5d0 !1.2d0 ! FIDUCIAL PRECISION PARAMETER FOR MCMC ANALYSIS (WITHOUT LENSING)
-  Real*8,parameter    :: q_linstep_mcmc = 1.d0 !40.d0                 ! FIDUCIAL PRECISION PARAMETER FOR MCMC ANALYSIS (WITHOUT LENSING)
+  Real*8,parameter    :: selection_sampling_bessel_fid = 3.d0 ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
+  Real*8,parameter    :: q_linstep_fid = 0.3d0                ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
+  Real*8,parameter    :: k_max_tau0_over_l_max_fid = 20.d0    ! FIDUCIAL PRECISION PARAMETER FOR FISHER ANALYSIS
+  Real*8,parameter    :: selection_sampling_bessel_mcmc = 1.2d0 ! FIDUCIAL PRECISION PARAMETER FOR MCMC ANALYSIS (WITHOUT LENSING)
+  Real*8,parameter    :: q_linstep_mcmc = 40.d0                 ! FIDUCIAL PRECISION PARAMETER FOR MCMC ANALYSIS (WITHOUT LENSING)
   Real*8,parameter    :: k_max_tau0_over_l_max_mcmc = 2.d0      ! FIDUCIAL PRECISION PARAMETER FOR MCMC ANALYSIS (WITHOUT LENSING)
 
   !##################
