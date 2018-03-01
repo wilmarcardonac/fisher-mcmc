bafac980ade92db0037d5bba86371657d59005cb
Wilmar Cardona <wcardona@hydra0.localdomain>
2018-03-01 19:45:27 +0100

    THIS VERSION SEEMS TO WORK BUT I AM TESTING IT

---
+++ output/compute_covariance_matrix_CMB.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = np.loadtxt('mcmc_output.txt')
+
+for index in range(len(data[:,5])):
+    data[index,5] = np.log(1e1**1e1*data[index,5])
+
+Cov = np.cov(data[:,2:],rowvar=0)
+
+np.savetxt('covariance_matrix.txt',Cov,fmt='%.10E',header=' ',comments='# ')
+
+exit()
