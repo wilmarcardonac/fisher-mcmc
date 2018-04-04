9ec4cb36ffc6f03baf4f6f9f64d4a6a4eb4ab9f1
Wilmar Cardona <wcardona@hydra0.localdomain>
2018-04-04 11:21:48 +0200

    SOME LINES ADDED TO CHANGE NAMES OF CHAINS FILES BEFORE COMPUTING STATISITCS

--- fiducial.f90
+++ fiducial.f90
@@ -19,12 +19,12 @@ Module fiducial
   Real*8,parameter :: deg_ncdm = 1.d0
   Real*8,parameter :: tau = 5.96d-2
   Real*8,parameter :: nc_bias_b0 = 1.0d0
-  Real*8,parameter :: cs2_fld = 3.333334d0 ! 1.d0 ; 1.d-4 ; 1.d-6 ; 3.333334d0 ; 3.3334d0 ; 4.3d0 
+  Real*8,parameter :: cs2_fld = 1.d0 ! 1.d0 ; 1.d-4 ; 1.d-6 ; 3.333334d0 ; 3.3334d0 ; 4.3d0 
   Real*8,parameter :: w0_fld = -8.0d-1 
   !Real*8,parameter :: wa_fld = 0.d0 
   Real*8,parameter :: e_pi = 0.0d0
-  Real*8,parameter :: f_pi = 5.0d0 ! 0.d0 ; 5.d0
-  Real*8,parameter :: g_pi = 1.0d0 ! 0.d0 ; 1.d0 ! THIS IS ACTUALLY log10 g_pi
+  Real*8,parameter :: f_pi = 0.0d0 ! 0.d0 ; 5.d0
+  Real*8,parameter :: g_pi = 0.0d0 ! 0.d0 ; 1.d0 ! THIS IS ACTUALLY log10 g_pi
 
 
   Character(len=*),parameter :: param_name_omega_b = 'omega_b'
@@ -111,11 +111,11 @@ Module fiducial
   !################
 
   Integer*4,parameter    :: number_iterations = 8000 !11000000        ! TOTAL NUMBER OF ITERATIONS IN MCMC RUN
-  Integer*4,parameter    :: number_of_parameters = 11       ! NUMBER OF COSMOLOGICAL PARAMETERS: 10 FOR DEA MODEL ONLY INCLUDING e_pi; 11 FOR DEA MODEL INCLUDING 
+  Integer*4,parameter    :: number_of_parameters = 10       ! NUMBER OF COSMOLOGICAL PARAMETERS: 10 FOR DEA MODEL ONLY INCLUDING e_pi; 11 FOR DEA MODEL INCLUDING 
   ! f_pi and g_pi; 12 FOR DEA MODEL INCLUDING e_pi, f_pi, and g_pi; 11 FOR DEA MODEL ONLY INCLUDING e_pi BUT JOINTLY ANALYSING NUMBER COUNTS AND CMB DATA; 
   ! 12 FOR DEA MODEL INCLUDING  f_pi and g_pi BUT JOINTLY ANALYSING NUMBER COUNTS AND CMB DATA; 13 FOR DEA MODEL INCLUDING e_pi, f_pi, and g_pi BUT JOINTLY ANALYSING 
   ! NUMBER COUNTS AND CMB DATA    
-  Integer*4,parameter    :: DEA_MODEL = 2 ! 1: DEA MODEL ONLY INCLUDING e_pi; 2: DEA MODEL INCLUDING f_pi and g_pi; 3: DEA MODEL INCLUDING e_pi, f_pi, g_pi 
+  Integer*4,parameter    :: DEA_MODEL = 1 ! 1: DEA MODEL ONLY INCLUDING e_pi; 2: DEA MODEL INCLUDING f_pi and g_pi; 3: DEA MODEL INCLUDING e_pi, f_pi, g_pi 
   Integer*4,parameter    :: number_DEA_parameters = DEA_MODEL ! NUMBER OF DEA MODEL PARAMETERS
   Integer*4,parameter    :: jumping_factor_update = 100    ! STEPS TAKEN BEFORE UPDATING JUMPING FACTOR (IF NEEDED)
   Integer*4,parameter    :: covariance_matrix_update = 0 !5000!10000 ! STEPS TAKEN BEFORE UPDATING COVARIANCE MATRIX (IF NEEDED)
@@ -130,14 +130,14 @@ Module fiducial
 
   Character*16,parameter :: phrase = 'randomizer'       ! PHRASE NEEDED BY NUMBER RANDOM GENERATOR
   ! MODEL 1 NUMBER COUNTS ALONE
-!  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
-!       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   e_pi   ']
+  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
+       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   e_pi   ']
   ! MODEL 1 NUMBER COUNTS + CMB 
 !  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
 !       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   e_pi   ','   tau    ']
   ! MODEL 2 NUMBER COUNTS ALONE
-  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
-       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   f_pi   ','   g_pi   ']
+!  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
+!       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   f_pi   ','   g_pi   ']
   ! MODEL 2 NUMBER COUNTS + CMB
 !  Character(len=10),dimension(number_of_parameters), parameter :: paramnames = ['omega_b   ','omega_cdm ','   n_s    ',&
 !       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   f_pi   ','   g_pi   ','   tau    ']
@@ -149,14 +149,14 @@ Module fiducial
 !       '   A_s    ','   H0     ','  m_ncdm  ','nc_bias_b0','cs2_fld   ','w0_fld    ','   e_pi   ','   f_pi   ','   g_pi   ',&
 !'   tau    ']
   ! MODEL 1 NUMBER COUNTS ALONE
-!  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
-!       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','e_{\pi}     ']
+  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
+       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','e_{\pi}     ']
   ! MODEL 1 NUMBER COUNTS + CMB
 !  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
 !       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','e_{\pi}     ','\tau        ']
   ! MODEL 2 NUMBER COUNTS ALONE
-  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
-       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','f_{\pi}     ','g_{\pi}     ']
+!  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
+!       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','f_{\pi}     ','g_{\pi}     ']
   ! MODEL 2 NUMBER COUNTS + CMB
 !  Character(len=12),dimension(number_of_parameters), parameter :: latexname = ['\omega_b    ','\omega_{cdm}','n_s         ',&
 !       'A_s         ','H_0         ','m_{\nu}     ','b_0         ','c_s^2       ','w_0         ','f_{\pi}     ','g_{\pi}     ',&
@@ -172,10 +172,10 @@ Module fiducial
   Logical,parameter      :: using_inverse_fisher_matrix = .false. !.true. !  USE INVERSE OF FISHER MATRIX AS A COVARIANCE MATRIX IF SET IT TRUE  
   Logical,parameter      :: do_mcmc_analysis = .true.    ! DO MCMC ANALYSIS IF SET IT TRUE
   Logical,parameter      :: start_from_fiducial = .false.    ! START MCMC ANALYSIS FROM FIDUCIAL POINT IF SET IT TRUE
-  Logical,parameter      :: start_from_bestfit = .false.    ! START MCMC ANALYSIS FROM BESTFIT IF SET IT TRUE
+  Logical,parameter      :: start_from_bestfit = .true.    ! START MCMC ANALYSIS FROM BESTFIT IF SET IT TRUE
   Logical,parameter      :: testing_Gaussian_likelihood = .false.  ! TEST GAUSSIAN LIKELIHOOD IF SET IT TRUE
   Logical,parameter      :: adjusting_covariance_matrix = .false.!.true.  ! UPDATE JUMPING FACTOR AND COVARIANCE MATRIX IF SET IT TRUE
-  Logical,parameter      :: read_covariance_matrix_from_file = .false. !.true. ! READ COVARIANCE MATRIX FROM FILE IF SET IT TRUE
+  Logical,parameter      :: read_covariance_matrix_from_file = .true. !.true. ! READ COVARIANCE MATRIX FROM FILE IF SET IT TRUE
   Logical,parameter      :: use_getdist = .false. ! USE GETDIST WHEN RUNNIG THE CODE IF SET IT TRUE
   Logical,parameter      :: multiple_chains = .true. !.true.!.false. ! USED TO RUN SEVERAL CHAINS WITH SAME COVARIANCE MATRIX IF SET IT TRUE
   Logical,parameter      :: use_only_autocorrelations = .false. ! COMPUTE LIKELIHOOD INCLUDING ONLY AUTOCORRELATIONS IF SET IT TRUE
