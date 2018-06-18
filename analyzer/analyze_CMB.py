b24c64f8d0383a34199604555fb2a36374b37155
Wilmar Cardona <wcardona@hydra0.localdomain>
2018-06-18 11:55:55 +0200

    CODE SEEMS TO WORK ALONG WITH FAKE PLANCK LIKELIHOOD

--- analyzer/analyze_CMB.py
+++ analyzer/analyze_CMB.py
@@ -1,5 +1,12 @@
 from getdist import loadMCSamples,plots,covmat
 import numpy as np
+import os,fnmatch
+
+filenames = fnmatch.filter(os.listdir("../output/chains/"),"mcmc_*.txt")
+
+for index in range(len(filenames)):
+
+    os.rename("../output/chains/"+str(filenames[index]),"../output/chains/mcmc_final_output_"+str(index+1)+".txt")
 
 number_of_parameters = 11
 
@@ -10,12 +17,6 @@ samples = loadMCSamples('../output/chains/mcmc_final_output',settings={'ignore_r
 g = plots.getSinglePlotter()
 
 g.settings.rcSizes(axes_fontsize = 2,lab_fontsize = 7)
-
-g.triangle_plot(samples,filled=True)
-
-g.export('../output/chains/triangle_figure.pdf')
-
-print 'TRIANGLE PLOT CREATED'
 
 p = samples.getParams()
 
@@ -55,11 +56,77 @@ f = plots.getSubplotPlotter()
 
 f.settings.rcSizes(axes_fontsize = 2,lab_fontsize = 7)
 
-f.plots_1d(samples,['omega_b','omega_cdm','n_s','A_s','H0','m_ncdm','nc_bias_b0','cs2_fld','w0_fld','e_pi','tau'])#,markers=[2.225e-2,1.198e-1,9.645e-1,2.20652e-9,6.727e1,6.0e-2,1.],nx=3)
+f.plots_1d(samples,['omega_b','omega_cdm','n_s','ln1010As','H0','m_ncdm','nc_bias_b0','logcs2fld','w0_fld','e_pi','tau'])#,markers=[2.225e-2,1.198e-1,9.645e-1,2.20652e-9,6.727e1,6.0e-2,1.],nx=3)
 
 f.export('../output/chains/1D_plots.pdf')
 
 print '1D PLOTS CREATED'
+
+g.triangle_plot(samples,params=['omega_b','omega_cdm','n_s','ln1010As','H0','m_ncdm','nc_bias_b0','logcs2fld','w0_fld','e_pi','tau'],filled=True,contour_colors=['blue'],legend_labels=['without lensing'],legend_loc='upper right')
+
+for ax in g.subplots[:,0]:
+    ax.axvline(2.218e-2,color='black',ls='--')
+
+for ax in g.subplots[1:,1]:
+    ax.axvline(1.205e-1,color='black',ls='--')
+
+for ax in g.subplots[2:,2]:
+    ax.axvline(9.619e-1,color='black',ls='--')
+    
+for ax in g.subplots[3:,3]:
+    ax.axvline(3.056,color='black',ls='--')
+
+for ax in g.subplots[4:,4]:
+    ax.axvline(66.93,color='black',ls='--')
+
+for ax in g.subplots[5:,5]:
+    ax.axvline(6.e-2,color='black',ls='--')
+
+for ax in g.subplots[6:,6]:
+    ax.axvline(1.,color='black',ls='--')
+
+for ax in g.subplots[7:,7]:
+    ax.axvline(0.,color='black',ls='--')
+
+for ax in g.subplots[8:,8]:
+    ax.axvline(-0.8,color='black',ls='--')
+
+for ax in g.subplots[9:,9]:
+    ax.axvline(0.,color='black',ls='--')
+
+for ax in g.subplots[9,0:9]:
+    ax.axhline(0.,color='black',ls='dotted')
+
+for ax in g.subplots[1,0:1]:
+    ax.axhline(1.205e-1,color='black',ls='dotted')
+
+for ax in g.subplots[2,0:2]:
+    ax.axhline(9.619e-1,color='black',ls='dotted')
+
+for ax in g.subplots[3,0:3]:
+    ax.axhline(3.056,color='black',ls='dotted')
+
+for ax in g.subplots[4,0:4]:
+    ax.axhline(66.93,color='black',ls='dotted')
+
+for ax in g.subplots[5,0:5]:
+    ax.axhline(6.e-2,color='black',ls='dotted')
+
+for ax in g.subplots[6,0:6]:
+    ax.axhline(1.,color='black',ls='dotted')
+
+for ax in g.subplots[7,0:7]:
+    ax.axhline(0.,color='black',ls='dotted')
+
+for ax in g.subplots[8,0:8]:
+    ax.axhline(-0.8,color='black',ls='dotted')
+
+for ax in g.subplots[9,0:9]:
+    ax.axhline(0.,color='black',ls='dotted')
+
+g.export('../output/chains/triangle_figure.pdf')
+
+print 'TRIANGLE PLOT CREATED'
 
 covariance_matrix = samples.getCov(pars=[0,1,2,11,4,5,6,12,8,9,10])#nparam=number_of_parameters)
 
--- launcher_12_reservation.sh
+++ launcher_12_reservation.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-#SBATCH --partition=batch
+#SBATCH --partition=batch3
 #SBATCH --cpus-per-task=12
 #SBATCH -J MCMC-no-lensing
 #SBATCH -n 1
 #SBATCH -N 1
-#SBATCH -t 5-20:00:00
+#SBATCH -t 2-22:00:00
 #SBATCH --mail-user=wilmar.cardona@csic.es
 #SBATCH --mail-type=ALL
 #SBATCH -o job-%J.out
+#SBATCH --reservation=wcardona_6
 
 l=`uname -n`
 h=`squeue -w $l -O jobid -h`
